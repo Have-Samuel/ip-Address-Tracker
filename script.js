8fc03493c8853ef1da0db2475871ab74460108da
Have Samuel <samiehave9@gmail.com>
2024-01-28 20:05:17 +0300

    Fetching Function

---
+++ script.js
@@ -0,0 +1,4 @@
+// Display of the Ip address, location and timezone of the user
+// using the ipify API
+// // using IP Geolocation API by IPify https://geo.ipify.org/
+// // using LeafletJS https://leafletjs.com/
