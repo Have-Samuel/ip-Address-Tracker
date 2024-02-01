7ba58dba9a2f746ac33f4434eae09980942f093a
Have Samuel <samiehave9@gmail.com>
2024-02-01 17:33:40 +0300

    Form validation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Form validation
+const form = document.querySelector("#form");
+const input = document.querySelector(".input");
+
+form.addEventListener("submit", (e) => {
+  // e.preventDefault(); // Prevents the form from submitting
+  e.preventDefault();
+});
 // Display of the Ip address, location and timezone of the user
 // using the ipify API
 // // using IP Geolocation API by IPify https://geo.ipify.org/
