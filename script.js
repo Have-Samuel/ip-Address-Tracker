7301b9a3c0c8775fbd51747b7536b33699dbc291
Have Samuel <samiehave9@gmail.com>
2024-02-01 17:52:35 +0300

    Writig the form function

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const form = document.querySelector("#form");
 const input = document.querySelector(".input");
 
 form.addEventListener("submit", (e) => {
-  // e.preventDefault(); // Prevents the form from submitting
   e.preventDefault();
+  const inputValue = input.value;
+
+
 });
+
 // Display of the Ip address, location and timezone of the user
 // using the ipify API
 // // using IP Geolocation API by IPify https://geo.ipify.org/
