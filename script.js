0e06d727cdf2a8a7f29ca669be17468bf19260e6
Have Samuel <samiehave9@gmail.com>
2024-02-05 18:11:27 +0300

    Editing according to Linters

--- script.js
+++ script.js
@@ -1,60 +1,58 @@
 // pull from different sources
-const api_url = "https://geo.ipify.org/api/v2?apiKey=at_D2SmDOVIsbseigGDuGmJTpMle90Da";
+const apiUrl = 'https://geo.ipify.org/api/v2?apiKey=at_D2SmDOVIsbseigGDuGmJTpMle90Da';
 const apiKey = 'at_D2SmDOVIsbseigGDuGmJTpMle90Da';
-let current_version = "v2";
-
-// Form validation
-const form = document.querySelector("#form");
-const input = document.querySelector(".input");
+const currentVersion = 'v2';
 
 // Display of the Ip address, location and timezone of the user
-  const current_ip = document.querySelector('.ip-address');
-  const current_location = document.querySelector('.location');
-  const timezone = document.querySelector('.timezone');
-  const isp = document.querySelector('.isp');
-  const entered_ip = document.querySelector('.input');
-  const search_btn = document.querySelector('#search-btn');
+const currentIp = document.querySelector('.ip-address');
+const currentLocation = document.querySelector('.location');
+const timeZone = document.querySelector('.timezone');
+const isp = document.querySelector('.isp');
+const enteredIp = document.querySelector('.input');
+const searchBtn = document.querySelector('#search-btn');
+const { L } = window;
 
 const map = L.map('map', {
   center: [0, 0],
   zoom: 0,
   layers: [
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      // maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }),
   ],
 });
 
 // Function for the longitude and latitude when the user enters a location
 // when the page loads this function shd create a map with the user's location
-updateLoaction = (lat, lng) => {
-  map.setView([lat, lng], 13);
+const updateLoaction = (lat, lng) => {
+  map.setView([lat, lng], 19);
   L.marker([lat, lng]).addTo(map);
 };
 
 // Function that shows the Ip address, location and timezone of the user
-showLocation = (default_ip) => {
-  if (default_ip === undefined) {
-    let ip_url = `${api_url}${current_version}?apiKey=${apiKey}`;
+const showLocation = (defaultIp) => {
+  let ipUrl;
+  if (defaultIp === undefined) {
+    ipUrl = `${apiUrl}${currentVersion}?apiKey=${apiKey}`;
   } else {
-    let ip_url = `${api_url}${current_version}?apiKey=${apiKey}&ipAddress=${default_ip}`;
+    ipUrl = `${apiUrl}${currentVersion}?apiKey=${apiKey}&ipAddress=${defaultIp}`;
   }
   // fetch the data from the api
-  fetch(ip_url)
+  fetch(ipUrl)
     .then((response) => response.json())
     // display the data
     .then((data) => {
-      current_ip.textContent = data.ip;
-      current_location.textContent = `${data.location.country}, ${data.location.region},`
-      timezone.textContent = `UTC ${data.location.timezone}`;
-      isp.textContent = data.isp;
+      currentIp.innerHTML = data.ip;
+      currentLocation.innerHTML = `${data.location.country}, ${data.location.region} ${data.location.postalCode}`;
+      timeZone.innerHTML = `UTC ${data.location.timezone}`;
+      isp.innerHTML = data.isp;
 
       // update the map with the user's location
-      updateLoaction([data.location.lat, data.location.lng])
+      updateLoaction([data.location.lat, data.location.lng]);
     })
     .catch((error) => console.log('Oops! Something went wrong', error));
-}
+};
 // call the function that shows the Ip address, location and timezone of the user
 showLocation();
 
@@ -62,14 +60,14 @@ showLocation();
 document.addEventListener('DOMContentLoaded', updateLoaction(0, 0));
 
 // Button to search for the location of the entered Ip address
-search_btn.addEventListener('click', (e) => {
+searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  showLocation(entered_ip.value);
+  showLocation(enteredIp.value);
   // if statement to check if the input is empty
-  if (entered_ip.value ==! '' && entered_ip.value ==! null) {
-    showLocation(entered_ip.value);
+  if (enteredIp.value !== '' && enteredIp.value !== null) {
+    showLocation(enteredIp.value);
     return;
   }
   alert('Please enter a valid IP address');
-  entered_ip.value = '';
+  enteredIp.value = '';
 });
