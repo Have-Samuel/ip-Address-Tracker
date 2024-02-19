dabe5520a283e57c785d3c5031f6c5ef54d7457f
Have Samuel <samiehave9@gmail.com>
2024-02-19 16:21:28 +0300

    Delete the unwanted code

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ const map = L.map('map', {
   zoom: 13,
   layers: [
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      // maxZoom: 19,
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }),
   ],
